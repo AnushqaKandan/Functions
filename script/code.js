970dd9018d9f694ad64551a7910f2dd8ea1f8e92
Name <anushqakandan03@gmail.com>
2024-05-23 09:37:38 +0200

    Functions

---
+++ script/code.js
@@ -0,0 +1,17 @@
+function greet(name){
+    console.log("Hello"+ " " + name);
+}
+greet("Anushqa")
+
+function multiply(a, b){
+    console.log(a*b);
+}
+multiply(4, 5)
+
+function tableOfFive(number){
+ let result = number * 5;
+ console.log(result);
+ return result
+}
+
+tableOfFive(7)
